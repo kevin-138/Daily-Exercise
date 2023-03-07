3170be5137c7653b1eb8462736b880c95323aef2
Kevin Goutama <kgoutama.kg@gmail.com>
2023-03-07 11:54:10 +0700

    First commit

--- app/src/main/java/com/kevin/dailyexercise/LoginScreen.kt
+++ app/src/main/java/com/kevin/dailyexercise/LoginScreen.kt
@@ -3,7 +3,7 @@ package com.kevin.dailyexercise
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class LoginScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
